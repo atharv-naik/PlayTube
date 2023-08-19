e1d2aa36acd93cef3570b12116dfa38ea2cc67f9
atharv-naik <atharvn317@gmail.com>
2023-08-19 17:33:27 +0530

    set override True to load env from .env file

--- playtube/settings.py
+++ playtube/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True) # Load environment variables from .env file
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
