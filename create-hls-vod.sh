85a717a00fe3cc77069e48f1dafd860315bbc22a
atharv-naik <atharvn317@gmail.com>
2023-08-29 14:16:23 +0530

    lower renditions number to reduce transcoding load on low resource systems

--- create-hls-vod.sh
+++ create-hls-vod.sh
@@ -10,10 +10,10 @@ echo "input flags ${1} ${2} ${3}"
 renditions=(
 # resolution  bitrate  audio-rate
   "426x240    400k    64k"
-  "640x360    800k     96k"
+  # "640x360    800k     96k"
   "842x480    1400k    128k"
   "1280x720   2800k    128k"
-  "1920x1080  5000k    192k"
+  # "1920x1080  5000k    192k"
 )
 
 segment_target_duration=10       # try to create a new segment every X seconds
