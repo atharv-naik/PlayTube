3bc6ec266e450c3c01442455d7e29aaa3f85414f
atharv-naik <atharvn317@gmail.com>
2024-05-29 13:52:56 +0530

    Fix: fixes crash when ip not found in `UTMTrackerMiddleware`

--- utm_tracker/middleware.py
+++ utm_tracker/middleware.py
@@ -4,6 +4,15 @@ from django.utils.deprecation import MiddlewareMixin
 from .models import Traffic, Source, SiteHit
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 class UTMTrackerMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # ignore all admin, static, media and api requests
